8bd10c8a036597995ff1453715c26a22f753db8a
matthewogtong <ogtongm@gmail.com>
2021-11-07 18:17:46 -0500

    Add new model Category for AnnieService to parse

---
+++ app/src/main/java/com/example/anniegif/model/Category.kt
@@ -0,0 +1,4 @@
+package com.example.anniegif.model
+
+class Category {
+}
--- app/src/main/java/com/example/anniegif/repo/remote/RetrofitInstance.kt
+++ app/src/main/java/com/example/anniegif/repo/remote/RetrofitInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitInstance {
 
-    private const val BASE_URL = "https://nekos.best/api/vi/"
+    private const val BASE_URL = "https://nekos.best/api/v1/"
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
