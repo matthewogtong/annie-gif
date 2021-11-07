e721449ec1e25b26ab4531a3524109432725f8d1
matthewogtong <ogtongm@gmail.com>
2021-11-07 16:47:37 -0500

    Add Internet uses permissions to manifests file

---
+++ app/src/main/java/com/example/anniegif/repo/remote/AnnieService.kt
@@ -0,0 +1,4 @@
+package com.example.anniegif.repo.remote
+
+class AnnieService {
+}
--- app/src/main/java/com/example/anniegif/repo/remote/RetrofitInstance.kt
+++ app/src/main/java/com/example/anniegif/repo/remote/RetrofitInstance.kt
@@ -3,9 +3,9 @@ package com.example.anniegif.repo.remote
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-class RetrofitInstance {
+object RetrofitInstance {
 
-    private val BASE_URL = "https://nekos.best/api/vi/"
+    private const val BASE_URL = "https://nekos.best/api/vi/"
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
