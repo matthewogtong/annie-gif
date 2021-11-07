ccf49d62c6bdf1c9cef921fcdf7a537e43c436b8
matthewogtong <ogtongm@gmail.com>
2021-11-06 20:06:18 -0400

    Add model, repo, view and viewmodel packages

--- app/src/main/java/com/example/anniegif/view/MainActivity.kt
+++ app/src/main/java/com/example/anniegif/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.anniegif
+package com.example.anniegif.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.anniegif.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
