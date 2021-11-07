2c90332415e23139fbe24411d60afd452d2c2fd0
matthewogtong <ogtongm@gmail.com>
2021-11-07 18:49:25 -0500

    Set up Category Model with proper object data types

--- app/src/main/java/com/example/anniegif/model/Category.kt
+++ app/src/main/java/com/example/anniegif/model/Category.kt
@@ -1,5 +1,215 @@
 package com.example.anniegif.model
 
-class Category {
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
-}
+data class Category (
+    val baka: Baka,
+    val bite: Bite,
+    val blush: Blush,
+    val bored: Bored,
+    val cry: Cry,
+    val cuddle: Cuddle,
+    val dance: Dance,
+    val facepalm: Facepalm,
+    val feed: Feed,
+    val happy: Happy,
+    val highfive: Highfive,
+    val hug: Hug,
+    val kiss: Kiss,
+    val laugh: Laugh,
+    val nekos: Nekos,
+    val pat: Pat,
+    val poke: Poke,
+    val pout: Pout,
+    val shrug: Shrug,
+    val slap: Slap,
+    val sleep: Sleep,
+    val smile: Smile,
+    val smug: Smug,
+    val stare: Stare,
+    val think: Think,
+    val thumbsup: Thumbsup,
+    val tickle: Tickle,
+    val wave: Wave,
+    val wink: Wink
+)
+
+data class Baka (
+    val min: String,
+    val max: String,
+    val format: String
+    )
+
+data class Bite (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Blush (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Bored (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Cry (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Cuddle (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Dance (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Facepalm (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Feed (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Happy (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Highfive (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Hug (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Kiss (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Laugh (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Nekos (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Pat (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Poke (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Pout (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Shrug (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Slap (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Sleep (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Smile (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Smug (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Stare (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Think (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Thumbsup (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Tickle (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Wave (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+data class Wink (
+    val min: String,
+    val max: String,
+    val format: String
+)
+
+
+
+
+
