d0d636fa6e84528462bfca6c3cfffafacaed61c0
matthewogtong <ogtongm@gmail.com>
2021-11-08 11:15:37 -0500

    Add new suspend function getGIFS to AnnieService

--- app/src/main/java/com/example/anniegif/repo/AnnieRepo.kt
+++ app/src/main/java/com/example/anniegif/repo/AnnieRepo.kt
@@ -1,6 +1,7 @@
 package com.example.anniegif.repo
 
 import com.example.anniegif.model.Categories
+import com.example.anniegif.model.GIFS
 import com.example.anniegif.repo.remote.RetrofitInstance
 import retrofit2.Response
 import java.util.*
@@ -8,6 +9,10 @@ import java.util.*
 object AnnieRepo {
     suspend fun getCategories(): Response <Categories> {
         return RetrofitInstance.annieService.getCategories()
+    }
+
+    suspend fun getGIFS(): Response <GIFS> {
+        return RetrofitInstance.annieService.getGIFS()
     }
 
 }
