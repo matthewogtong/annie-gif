867e1e5b40ebb3c4fc3ba810f7a178214243d8db
matthewogtong <ogtongm@gmail.com>
2021-11-08 17:43:01 -0500

    Adjust Gifs model

--- app/src/main/java/com/example/anniegif/model/Gifs.kt
+++ app/src/main/java/com/example/anniegif/model/Gifs.kt
@@ -6,8 +6,13 @@ import com.squareup.moshi.Json
 import kotlinx.android.parcel.Parcelize
 
 @JsonClass(generateAdapter = true)
-@Parcelize
 data class Gifs (
-    val url: String?,
-    @Json(name = "anime_name") val animeName: String?
-    ) : Parcelable
+    val url: Url
+)
+
+@JsonClass(generateAdapter = true)
+@Parcelize
+data class Url (
+val url: String?,
+@Json(name = "anime_name") val animeName: String?
+) : Parcelable
