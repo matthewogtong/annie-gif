58134d653adecd68082eb9489209fca7b773eae9
matthewogtong <ogtongm@gmail.com>
2021-11-07 21:14:59 -0500

    Declare viewModel and biniding variables in MainActivity

--- app/src/main/java/com/example/anniegif/view/MainActivity.kt
+++ app/src/main/java/com/example/anniegif/view/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.anniegif.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.anniegif.R
+import com.example.anniegif.databinding.ActivityMainBinding
+import com.example.anniegif.viewmodel.AnnieViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<AnnieViewModel>()
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/example/anniegif/viewmodel/AnnieViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.anniegif.viewmodel
+
+class AnnieViewModel {
+
+}
