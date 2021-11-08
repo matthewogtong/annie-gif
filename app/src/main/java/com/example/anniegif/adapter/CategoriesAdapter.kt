4fcb260810c9d08955bbf748ebf39d4aea664723
matthewogtong <ogtongm@gmail.com>
2021-11-08 12:07:20 -0500

    Add override functions to CategoriesAdapter

--- app/src/main/java/com/example/anniegif/adapter/CategoriesAdapter.kt
+++ app/src/main/java/com/example/anniegif/adapter/CategoriesAdapter.kt
@@ -1,4 +1,46 @@
 package com.example.anniegif.adapter
 
-class CategoriesAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.anniegif.databinding.CategoryNameBinding
+import com.example.anniegif.model.Categories
+
+class CategoriesAdapter : RecyclerView.Adapter<CategoriesAdapter.CategoryViewHolder>() {
+    private val categories = mutableListOf<Categories>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        return CategoryViewHolder.getInstance(parent)
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        holder.loadInfo(categories[position])
+    }
+
+    override fun getItemCount(): Int {
+        return categories.size
+    }
+
+    class CategoryViewHolder(
+        private val binding: CategoryNameBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun loadInfo(category: Category) = with(binding) {
+
+        }
+
+        companion object {
+            fun getInstance(parent:ViewGroup): CategoryViewHolder {
+                val binding = CategoryNameBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+
+                return CategoryViewHolder(binding)
+            }
+        }
+    }
+
+
 }
