db93d0e1000dd8f5b25b9cec29b1e659ded96c15
matthewogtong <ogtongm@gmail.com>
2021-11-08 11:11:44 -0500

    Add Parcelable to GIFS data class

--- app/src/main/java/com/example/anniegif/model/GIFS.kt
+++ app/src/main/java/com/example/anniegif/model/GIFS.kt
@@ -1,9 +1,13 @@
 package com.example.anniegif.model
 
+import android.os.Parcelable
 import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
 
 @JsonClass(generateAdapter = true)
+@Parcelize
 data class GIFS (
     val url: String?,
-    val anime_name: String?
-    )
+    @Json(name = "anime_name") val animeName: String?
+    ) : Parcelable
