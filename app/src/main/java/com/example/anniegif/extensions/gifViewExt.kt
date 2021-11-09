16f1335c8f022f9f7efae09f9e36d4ab9357bfcf
matthewogtong <ogtongm@gmail.com>
2021-11-09 13:42:10 -0500

    Remove all package and enable fresh start on project

--- app/src/main/java/com/example/anniegif/extensions/gifViewExt.kt
+++
@@ -1,8 +0,0 @@
-package com.example.anniegif.extensions
-
-import android.widget.ImageView
-import com.bumptech.glide.Glide
-
-fun ImageView.loadUrl(url: String) {
-    Glide.with(this).load(url).into(this)
-}
