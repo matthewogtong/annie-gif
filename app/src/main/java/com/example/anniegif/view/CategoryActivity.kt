0130ec6ce33904c70ec26f6783e0a1186ba069c6
matthewogtong <ogtongm@gmail.com>
2021-11-10 10:40:29 -0500

    Initialize path variable for a list of categories as strings

--- app/src/main/java/com/example/anniegif/view/CategoryActivity.kt
+++ app/src/main/java/com/example/anniegif/view/CategoryActivity.kt
@@ -8,8 +8,12 @@ import com.example.anniegif.viewmodel.GifViewModel
 
 class CategoryActivity : AppCompatActivity() {
 
-    private val viewModel by viewModels<GifViewModel>
+    private val viewModel by viewModels<GifViewModel>()
     private val binding by lazy { ActivityCategoryBinding.inflate(layoutInflater)}
+    private val path = arrayListOf("baka", "bite", "blush", "bored", "cry", "cuddle",
+        "dance", "facepalm", "feed", "happy", "highfive", "hug", "kiss", "laugh",
+        "pat", "poke", "pout", "shrug", "slap", "sleep", "smile", "smug", "stare",
+        "think", "thumbsup", "tickle", "wave", "wink")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
