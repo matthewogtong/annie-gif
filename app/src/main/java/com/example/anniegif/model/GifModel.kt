0d8e8c45e24accbba28a96c637ebc4a075ed6da8
matthewogtong <ogtongm@gmail.com>
2021-11-10 10:22:08 -0500

    Parcelize GifInfo

--- app/src/main/java/com/example/anniegif/model/GifModel.kt
+++ app/src/main/java/com/example/anniegif/model/GifModel.kt
@@ -1,15 +1,19 @@
 package com.example.anniegif.model
 
+import android.os.Parcelable
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import java.io.Serializable
+import kotlinx.android.parcel.Parcelize
 
 @JsonClass(generateAdapter = true)
 data class GifModel(
     val url: List<GifInfo>
-) : Serializable
+)
 
 @JsonClass(generateAdapter = true)
+@Parcelize
 data class  GifInfo(
     val url: String,
+    @Json(name = "anime_name")
     val anime_name: String
-)
+) : Parcelable
