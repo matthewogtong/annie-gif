a6b487e96acfa1e7935f959cbd555a6a03e4d90f
matthewogtong <ogtongm@gmail.com>
2021-11-10 10:39:38 -0500

    Replace MainActivity file with CategoryActivity and adjust relevant files

---
+++ app/src/main/java/com/example/anniegif/view/CategoryActivity.kt
@@ -0,0 +1,18 @@
+package com.example.anniegif.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.anniegif.databinding.ActivityCategoryBinding
+import com.example.anniegif.viewmodel.GifViewModel
+
+class CategoryActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<GifViewModel>
+    private val binding by lazy { ActivityCategoryBinding.inflate(layoutInflater)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
