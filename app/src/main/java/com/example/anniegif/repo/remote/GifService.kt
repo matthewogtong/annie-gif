60bdcf4d2b0b469a8e2a0a962961450e94e5ff07
matthewogtong <ogtongm@gmail.com>
2021-11-10 10:15:41 -0500

    Add Get and Path annotation for GifService

--- app/src/main/java/com/example/anniegif/repo/remote/GifService.kt
+++ app/src/main/java/com/example/anniegif/repo/remote/GifService.kt
@@ -1,5 +1,13 @@
 package com.example.anniegif.repo.remote
 
-interface GifService {
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
+interface GifService {
+    @GET("{path}")
+    suspend fun getPath(
+        @Path("path") pathParam: String,
+        @Query("amount") amount: Int
+    ) : Response<GifModel>
 }
