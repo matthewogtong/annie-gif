95ad655801971358714d9df260d89d3df906e651
matthewogtong <ogtongm@gmail.com>
2021-11-10 10:49:23 -0500

    Create initViews function and connect adapter to MainActivity

--- app/src/main/java/com/example/anniegif/view/CategoryActivity.kt
+++ app/src/main/java/com/example/anniegif/view/CategoryActivity.kt
@@ -1,8 +1,11 @@
 package com.example.anniegif.view
 
+import android.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.viewModels
+import com.example.anniegif.adapter.AnnieGifAdapter
 import com.example.anniegif.databinding.ActivityCategoryBinding
 import com.example.anniegif.viewmodel.GifViewModel
 
@@ -19,5 +22,22 @@ class CategoryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.spCategory.adapter = ArrayAdapter(this, R.layout.simple_list_item_1, path)
+
+        initViews()
+        viewModel.gifObj.observe(this){
+            (binding.rvGif.adapter as AnnieGifAdapter).updateGifInfoList(it)
+        }
+    }
+
+    private fun initViews() = with(binding) {
+        rvGif.adapter = AnnieGifAdapter()
+        btnSubmit.setOnClickListener {
+            if (etAmount.text.toString() != ""){
+                viewModel.fetchData(spCategory.selectedItem.toString(),etAmount.text.toString().toInt())
+            }else{
+                viewModel.fetchData(spCategory.selectedItem.toString(),0)
+            }
+        }
     }
 }
