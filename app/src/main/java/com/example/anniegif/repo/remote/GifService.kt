961a1822fc770ca39a89b3481d99431ce59e0cf8
matthewogtong <ogtongm@gmail.com>
2021-11-10 10:19:15 -0500

    Create data class for GifModel and GifInfo

--- app/src/main/java/com/example/anniegif/repo/remote/GifService.kt
+++ app/src/main/java/com/example/anniegif/repo/remote/GifService.kt
@@ -1,5 +1,7 @@
 package com.example.anniegif.repo.remote
 
+import com.example.anniegif.model.GifModel
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
