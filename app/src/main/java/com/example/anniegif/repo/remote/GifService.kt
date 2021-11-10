f188531b8b294e8d955ad3e65340ec2641723122
matthewogtong <ogtongm@gmail.com>
2021-11-10 10:12:46 -0500

    Initialize retrofit and gifservice in RetrofitInstance

---
+++ app/src/main/java/com/example/anniegif/repo/remote/GifService.kt
@@ -0,0 +1,5 @@
+package com.example.anniegif.repo.remote
+
+interface GifService {
+
+}
--- app/src/main/java/com/example/anniegif/repo/remote/RetrofitInstance.kt
+++ app/src/main/java/com/example/anniegif/repo/remote/RetrofitInstance.kt
@@ -1,5 +1,14 @@
 package com.example.anniegif.repo.remote
 
-object RetrofitInstance {
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
+
+object RetrofitInstance {
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl("https://nekos.best/api/v1/")
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+
+    val gifService: GifService = retrofit.create(GifService::class.java)
 }
